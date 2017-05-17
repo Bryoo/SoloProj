37a6be3aa52454aa05d64dccbe37ed1e3d09c264
Bryoo <bryantskisseu@gmail.com>
2017-05-17 06:38:25 +0300

    added people and dojo classes

--- people.py
+++ people.py
@@ -1,4 +1,5 @@
 class Person(object):
+
     pass
 
 class Fellow(Person):
--- room.py
+++ room.py
@@ -3,7 +3,6 @@ class Room(object):
     max = 0
     pass
 
-
 class Office(Room):
     def __init__(self, name):
         self.name = name
