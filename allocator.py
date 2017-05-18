8819cce3181ae916366464ceeeb96792c728e445
Bryoo <bryantskisseu@gmail.com>
2017-05-18 06:16:01 +0300

    worked on print room functionality

--- allocator.py
+++ allocator.py
@@ -8,7 +8,8 @@ class Dojo(object):
         self.room_name = []
         self.offices = {}
         self.livings = {}
-
+        self.fellow_unallocated = []
+        self.staff_unallocated = []
         self.staff_list = []
         self.fellow_list = []
 
@@ -36,11 +37,11 @@ class Dojo(object):
                 print("All entries exist hence no room created\n")
 
         else:
-            non_living_duplicates = [rm for rm in rooms if rm not in list(self.livings.keys())]
+            not_living_duplicates = [rm for rm in rooms if rm not in list(self.livings.keys())]
             living_duplicates = [rm for rm in rooms if rm in list(self.livings.keys())]
 
-            if non_living_duplicates:  # if there are no duplicates
-                for room in non_living_duplicates:
+            if not_living_duplicates:  # if there are no duplicates
+                for room in not_living_duplicates:
                     create_living = LivingSpace(room)
                     self.livings[create_living.name] = []
                     print("Living space ", room, " has been created")
@@ -70,14 +71,23 @@ class Dojo(object):
             self.assign_room(create_staff, need_room)
 
     def assign_room(self, person, need_room):
-        # check for vacant rooms by looping either the office or living spaces
-        empty_office_dict = self.check_vacant_room(self.offices, 6)
+        """ check for vacant rooms by looping either the office or living spaces"""
+        # assigning office rooms to all parties
+        try:
+            # check if there are vacant rooms in dictionary
+            empty_office_dict = self.check_vacant_room(self.offices, 6)
+            random_offices = random.choice(list(empty_office_dict))
+            self.offices[random_offices].append(person.name)
+            print(person.name, " has been assigned to ", random_offices)
 
-        random_offices = random.choice(list(empty_office_dict))
+        except BaseException as error:
+            if isinstance(person, Fellow):
+                self.fellow_unallocated.append(person)
+                print("Fellow", person.name, "added but there are no rooms to allocate ")
+            else:
+                self.staff_unallocated.append(person)
 
-        self.offices[random_offices].append(person.name)
-        print(person.name, " has been assigned to ", random_offices)
-
+        # Assigning living room
         if need_room:
             try:
                 empty_living_dict = self.check_vacant_room(self.livings, 4)
@@ -88,7 +98,11 @@ class Dojo(object):
                 print(person.name, " has been assigned to ", random_living)
 
             except BaseException as error:
-                print(error.__class__.__class__, ": ", error)
+                if error.__class__.__name__ == "IndexError":
+                    print("Person added but not allocated to a living space")  # (if staff dont print)
+                else:
+                    print(error.__class__.__class__, ": ", error)
+
                 # to create a new room and assign the person to that room
 
     def check_vacant_room(self, existing_dict, max):
@@ -104,6 +118,29 @@ class Dojo(object):
 
         return semi_full_dict
 
+    def print_room(self,is_office, rooms):
+        """ Prints out rooms and occupants"""
+        if is_office:
+            # get offices that are not in existing room dict
+            null_offices = [rm for rm in rooms if rm not in list(self.offices.keys())]
+            # get offices that are in existing room dict
+            existing_offices = [rm for rm in rooms if rm in list(self.offices.keys())]
+
+            all_offices = [null_offices, existing_offices]
+            return all_offices
+
+        else:
+            # get offices that are not in existing room dict
+            null_living = [rm for rm in rooms if rm not in list(self.livings.keys())]
+            # get offices that are in existing room dict
+            existing_living = [rm for rm in rooms if rm in list(self.livings.keys())]
+
+            all_livings = [null_living, existing_living]
+            return all_livings
+
+    def print_unallocated(self ):
+        pass
     # create_room office blue_office black_office
     # create_room living blue_living black_living
     # create_person bryoo muthama fellow yes
+    # print_room office blue_office black_office
